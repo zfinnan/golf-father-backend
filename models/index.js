8c7ae89e7c1d9c5bc434b27c9238a36c3e0fae72
zfinnan <63361320+zfinnan@users.noreply.github.com>
2021-01-27 00:06:19 -0500

    add process.env

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 // const { MONGO_URI } = require('../config/keys');
 // Mongo connection
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true
